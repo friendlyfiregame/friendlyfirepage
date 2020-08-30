d9c01d04accc835d16cdf82dd4997f72ec66fc1c
Edu <eduke@gmx.de>
2020-08-30 13:23:10 +0200

    change and remove some unnecessery stuff

--- config/site.config.js
+++ config/site.config.js
@@ -9,13 +9,13 @@ if (!ROOT) {
 
 const config = {
   // Your website's name, used for favicon meta tags
-  site_name: 'Post-Jam Games',
+  site_name: 'Friendly Fire',
 
   // Your website's description, used for favicon meta tags
-  site_description: 'A modern boilerplate for static website development',
+  site_description: 'Friendly Fire is a dialog-driven indie pixel platformer with metroidvania elements originally developed as a team effort for the game jam Ludum Dare #46 within three days. It is free to play an can be accessed via steam or browser.',
 
   // Your website's URL, used for sitemap
-  site_url: 'https://games.d-cay.de',
+  site_url: 'https://www.friendlyfiregame.com',
 
   // Google Analytics tracking ID (leave blank to disable)
   googleAnalyticsUA: '',
